e855777224cbfa71cb527ac223dfe9acaa2b0ad2
mohdrashadi-pro <moalrashadi@gmail.com>
2026-02-12 10:53:52 +0800

    ReOrder folder

---
+++ Learning/Level-1-Java-Basics/Variables  Data Types/src/Exercise1.java
@@ -0,0 +1,10 @@
+public class Exercise1 {
+    public static void main(String[] args){
+        String name = "";
+        int age;
+        double height;
+        char grade;
+        boolean student;
+
+    }
+}
---
+++ Learning/Level-1-Java-Basics/Variables  Data Types/src/IntegerType.java
@@ -0,0 +1,17 @@
+public class IntegerType {
+    public static void main(String[] args){
+        byte level =127;
+        short students = 5000;
+        int salary =  50000;
+        long worldPoublation = 8000000000L;
+
+        System.out.println("Level: " + level);
+        System.out.println("Student : "+ students);
+        System.out.println("salary : "+ salary);
+        System.out.println("world Poublation :"+ worldPoublation);
+
+
+
+
+    }
+}
